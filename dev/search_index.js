8b5604cf1c90d8f8cc18c4b8ab94d8c100cc902e
Documenter.jl <documenter@juliadocs.github.io>
2022-08-10 12:58:56 +0000

    build based on 7c3c73b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GRIBDatasets","category":"page"},{"location":"#GRIBDatasets","page":"Home","title":"GRIBDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GRIBDatasets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GRIBDatasets]","category":"page"}]
+}
