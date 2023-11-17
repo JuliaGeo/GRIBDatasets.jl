f629c68d96a290038772d5ab679ba82600295dee
Tristan Carion <tristancarion@gmail.com>
2023-11-17 18:02:08 +0100

    deploy docs on JuliaGeo

--- docs/make.jl
+++ docs/make.jl
@@ -12,11 +12,11 @@ DocMeta.setdocmeta!(GRIBDatasets, :DocTestSetup, setup; recursive=true)
 makedocs(;
     modules=[GRIBDatasets],
     authors="tcarion <tristan.carion@gmail.com> and contributors",
-    repo="https://github.com/tcarion/GRIBDatasets.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/JuliaGeo/GRIBDatasets.jl/blob/{commit}{path}#{line}",
     sitename="GRIBDatasets.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://tcarion.github.io/GRIBDatasets.jl",
+        canonical="https://JuliaGeo.github.io/GRIBDatasets.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -30,6 +30,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/tcarion/GRIBDatasets.jl",
+    repo="github.com/JuliaGeo/GRIBDatasets.jl",
     devbranch="main",
 )
